7d897a9649676f3e41c5e4adf86d5d9e5a2ef49b
Cris T <crisostomotorres@hotmail.com>
2019-12-23 17:44:58 -0300

    Even more JS

---
+++ Crisostomo/js3.js
@@ -0,0 +1,13 @@
+/*for (let i = prompt('Ingrese un numero menor de 50'); i >= 1; i--) {
+    for (let a = 0; a < i; a++) {
+        document.write(i)
+    }
+    document.write('<br>')
+}*/
+
+for (let i = prompt('Ingrese un numero NO mayor que 50'); i >= 50; i++) {
+    for (let a = 0; a < i; ++1) {
+        document.write(i)
+    }
+    document.write('<br>')
+}
