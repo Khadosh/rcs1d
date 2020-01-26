54489926ee8df5a821bcbc1193de3b7246a9d83f
CrisT <crisostomotorres@hotmail.com>
2020-01-26 20:27:36 -0300

    Library exercise

--- Crisostomo/registroLibreria.js
+++ Crisostomo/registroLibreria.js
@@ -1,1 +1,23 @@
-var listaDeLibrosDisponibles = [];
+var listaDeLibros = ['20000 leguas de viaje submarino', ' El Principito', ' Steve Jobs'];
+window.onload = function() {
+    var rentedBooks = document.getElementById('Libros');
+    listaDeLibros.forEach(function(book) {
+        var element = document.createElement('li');
+        element.innerText = book;
+        rentedBooks.appendChild(element);
+    });
+};
+
+// rent
+document.getElementById('alquilar').addEventListener('click', alquilar);
+
+
+function alquilar(titulo) {
+    listaDeLibros.indexOf('20000 leguas de viaje submarino');
+    listaDeLibros.splice(0);
+}
+
+// return
+function devolver(titulo) {
+
+}
