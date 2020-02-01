c7ec654751880480566c32e4f450516406543635
CrisT <crisostomotorres@hotmail.com>
2020-02-01 16:52:01 -0300

    added some arrow functions

--- Crisostomo/indexTaskList.js
+++ Crisostomo/indexTaskList.js
@@ -1,9 +1,67 @@
 let tasks = [];
+let td2Tasks = document.querySelector('#secondList');
+let tdTasks = document.querySelector('#firstList');
+let td3Tasks = document.querySelector('#thirdList');
 
+let addTask = () => {
+    let inputTextTask = document.querySelector('#inputTask').value;
+    tasks.push(inputTextTask);
+    document.querySelector('#inputTask').value = '';
+    listTasks();
+}
+
+let listTasks = () => {
+    tdTasks.innerHTML = '';
+    tasks.forEach(function(item, index) {
+        console.log(item);
+        tdTasks.innerHTML += `<td>${item}<button class="btn btn-primary" onclick="moveTaskToProgress(${index})">-></button></td>`;
+    })
+}
+
+let process = [];
+let moveTaskToProgress = (i) => {
+    process.push(tasks[i]);
+    tasks.splice(i, 1);
+    listTasks();
+    moveTasks();
+}
+
+let moveTasks = () => {
+    td2Tasks.innerHTML = '';
+    process.forEach(function(item, index) {
+        console.log(item);
+        td2Tasks.innerHTML += `<td>${item}<button class="btn btn-primary" onclick="moveTaskToDone(${index})">-></button></td>`;
+    })
+}
+
+moveTasks();
+
+let done = [];
+
+let moveTaskToDone2 = (i) => {
+    done.push(process[i]);
+    process.splice(i, 1);
+    moveTaskToProgress();
+    moveTaskToDone();
+
+}
+
+let moveTaskToDone = () => {
+    td3Tasks.innerHTML = '';
+    done.forEach(function(item, index) {
+        console.log(item);
+        td3Tasks.innerHTML += `<td>${item}(${index})</td>`;
+    })
+}
+
+moveTaskToDone();
+
+/*
 function btnAddFunction() {
     let text = document.querySelector("#inputTask").value;
-    let list = '<td>' + text + '</td>';
-    document.querySelector("#firstList").innerHTML = list;
+    let list = `<td> ${text} </td>`;
+    const nodo = JSON.parse(list);
+    document.querySelector("#firstList").appendChild(nodo);
 }
 
 function moveTaskInProgress() {
@@ -13,3 +71,4 @@ function moveTaskInProgress() {
 function moveTaskCompleted() {
 
 }
+*/
